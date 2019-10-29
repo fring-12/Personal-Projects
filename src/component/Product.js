e92e9ae57de4465291e76d7fe57c0a1f89734b02
Banedict Fring Drong <banedictfring12@gmail.com>
2019-10-30 01:38:33 +0600

    added cart

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
 export default class Product extends Component {
     render() {
+        const {id, title, img, price, inCart} = this.props.product;
         return (
-            <div>
-                <h3>hello from Product</h3>
-            </div>
+            <ProductWrapper class="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div
+                     className="img-container p-5" 
+                     onClick={()=>console.log("you clicked me on image")}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"/>
+                        </Link>
+                    </div>
+                    {/* <button className="cart-btn"></button> */}
+
+                </div>
+
+            </ProductWrapper>
         );
     }
 }
 
+const ProductWrapper = styled.div`
 
+
+`
