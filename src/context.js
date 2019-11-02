be5c2fd6fe37e9e80a026111f2215044bbfcbe24
Banedict Fring Drong <banedictfring12@gmail.com>
2019-11-02 23:40:59 +0600

    added add to cart

--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
     state ={
         products: [],
-        detailProduct: detailProduct 
+        detailProduct: detailProduct ,
+        cart:[]
     };
 componentDidMount() {
     this.setProducts();
@@ -37,8 +38,20 @@ handleDetail = id =>{
     });
 };
 
-addToCart = (id) =>{
-    console.log(`id is ${id}`);
+addToCart = id =>{
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(() =>{
+        return {products: tempProducts, cart:[...this.state.cart,
+            product]};
+    },()=>{console.log(this.state)});
+
 };
     render() {
         return (
