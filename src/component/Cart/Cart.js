9936406fe77567804ae91d13e9db6b48471b2a0c
fring.codesmith <banedictfring.codesmith@gmail.com>
2019-11-11 11:56:48 +0600

    last updated

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class cart extends Component {
                                     <Title name="your" title="cart"/>
                                      <CartColumns />
                                      <CartList value={value} />
-                                     <CartTotals value={value} />
+                                     <CartTotals value={value} history={this.props.history} />
                                 </React.Fragment>
                             );
                         } else{
