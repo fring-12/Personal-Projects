5b38431b67a3c78f9fd945da2d191a5dac3eb0c8
fring.codesmith <banedictfring.codesmith@gmail.com>
2019-11-17 15:46:48 +0600

    navbar prettier

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -32,7 +32,7 @@ export default class Navbar extends Component {
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
                             products
-                            </Link>
+                        </Link>
                     </li>
                     
                 </ul>
